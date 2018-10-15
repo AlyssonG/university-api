d63fd4b79ce64183bbfe5f8db055c6338eaaaab7
Alysson <alyssonggs@hotmail.com>
2018-10-15 18:53:35 -0300

    Added enpoints for professor

--- main.go
+++ main.go
@@ -12,10 +12,18 @@ import (
 )
 
 func main() {
+	logger := log.New(os.Stdout, "-- University API --", 0)
 	studentHandle := handle.StudentHandle{
-		Logger: log.New(os.Stdout, "-- University API --", 0),
+		Logger: logger,
 		Storage: &memory.StudentStorage{
 			Store: make(map[string]*university.Student),
+		},
+	}
+
+	professorHandle := handle.Professor{
+		Logger: logger,
+		Storage: &memory.ProfessorStorage{
+			Store: make(map[string]*university.Professor),
 		},
 	}
 
@@ -23,6 +31,11 @@ func main() {
 	http.HandleFunc("/student/new", studentHandle.SetStudent)
 	http.HandleFunc("/student/delete", studentHandle.DeleteStudent)
 	http.HandleFunc("/student/update", studentHandle.UpdateStudent)
+
+	http.HandleFunc("/professor", professorHandle.GetProfessor)
+	http.HandleFunc("/professor/new", professorHandle.SetProfessor)
+	http.HandleFunc("/professor/delete", professorHandle.DeleteProfessor)
+	http.HandleFunc("/professor/update", professorHandle.UpdateProfessor)
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("Server error", "err", err)
