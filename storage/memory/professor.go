cdff187aa0f118dc9c24493ffe7d3989b88d58fe
Alysson <alyssonggs@hotmail.com>
2018-10-15 14:46:56 -0300

    Changing name from func receiver

--- storage/memory/professor.go
+++ storage/memory/professor.go
@@ -13,18 +13,18 @@ type ProfessorStorage struct {
 }
 
 //Set creates or updates a professor record in memory.
-func (ss *ProfessorStorage) Set(professor *university.Professor) (string, error) {
+func (ps *ProfessorStorage) Set(professor *university.Professor) (string, error) {
 	if professor == nil {
 		return "", errors.New("Professor is nil. There's no data to store")
 	}
 
-	ss.Store[professor.Code] = professor
+	ps.Store[professor.Code] = professor
 	return professor.Code, nil
 }
 
 //Get retrives a professor record in memory.
-func (ss *ProfessorStorage) Get(professorCode string) (*university.Professor, error) {
-	professor, ok := ss.Store[professorCode]
+func (ps *ProfessorStorage) Get(professorCode string) (*university.Professor, error) {
+	professor, ok := ps.Store[professorCode]
 	if !ok {
 		return nil, errors.New("Empty result")
 	}
@@ -33,18 +33,18 @@ func (ss *ProfessorStorage) Get(professorCode string) (*university.Professor, er
 }
 
 //Delete deletes a professor record in memory.
-func (ss *ProfessorStorage) Delete(professorCode string) error {
-	if ss.Store[professorCode] == nil {
+func (ps *ProfessorStorage) Delete(professorCode string) error {
+	if ps.Store[professorCode] == nil {
 		return errors.New("There is no record for this id")
 	}
 
-	delete(ss.Store, professorCode)
+	delete(ps.Store, professorCode)
 	return nil
 }
 
-func (ss *ProfessorStorage) GetAll() ([]*university.Professor, error) {
+func (ps *ProfessorStorage) GetAll() ([]*university.Professor, error) {
 	response := []*university.Professor{}
-	for _, value := range ss.Store {
+	for _, value := range ps.Store {
 		response = append(response, value)
 	}
 	return response, nil
